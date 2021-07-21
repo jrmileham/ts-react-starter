807d3e5740ceabcfa03b942e1bc4f03deb597fdd
Johnny Mileham <johnny.mileham@unmind.com>
2021-07-21 20:01:42 +0100

    First component and some basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import pkg from '@package';
-
+import ExpenseItem from '@App/expense/components/ExpenseItem';
 /**
  * Main function
  */
 function App(): JSX.Element {
   return (
-    <div className="App"> 
+    <div className="App">
+      <ExpenseItem />
       <p>¡Hola world! Soy {pkg.name}</p>
     </div>
   );
---
+++ src/expense/components/ExpenseItem.tsx
@@ -0,0 +1,14 @@
+import './ExpenseItem.scss';
+
+function ExpenseItem(): JSX.Element {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
