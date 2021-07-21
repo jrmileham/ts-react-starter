a9205933e5cf51bb6da68986bec43ed52837e338
Johnny Mileham <johnny.mileham@unmind.com>
2021-07-21 20:57:54 +0100

    Using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import pkg from '@package';
-import ExpenseItem from '@App/expense/components/ExpenseItem';
+import ExpenseItem, { ExpenseItemProps } from '@App/expense/components/ExpenseItem';
 /**
  * Main function
  */
 function App(): JSX.Element {
+  const expenseDate = new Date(2021, 6, 20);
+  const expenseTitle = "Tech";
+  const expenseAmount = 353.52;
+
+  const expenses: (ExpenseItemProps & {key: number})[] = [
+    {
+      key: 2,
+      expenseDate: new Date(2021, 2, 14),
+      expenseTitle: "Vacaciones",
+      expenseAmount: 1003
+    },
+    {
+      key: 3,
+      expenseDate: new Date(2021, 5, 5),
+      expenseTitle: "Viajo",
+      expenseAmount: 500.25
+    }
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem />
-      <p>¡Hola world! Soy {pkg.name}</p>
+      <ExpenseItem {...{key: 0, expenseDate, expenseAmount, expenseTitle}}/>
+      <ExpenseItem
+        key={1}
+        expenseAmount={30} 
+        expenseDate={new Date(2021, 3, 30)} 
+        expenseTitle="Phone"
+      />
+      {expenses.map(expense => <ExpenseItem {...expense} />)}
     </div>
   );
 }
--- src/expense/components/ExpenseItem.tsx
+++ src/expense/components/ExpenseItem.tsx
@@ -1,12 +1,26 @@
+import { PropsWithChildren } from 'react';
 import './ExpenseItem.scss';
 
-function ExpenseItem(): JSX.Element {
+
+export type ExpenseItemProps = {
+  expenseDate: Date,
+  expenseTitle: string,
+  expenseAmount: number
+}
+
+function ExpenseItem(props: PropsWithChildren<ExpenseItemProps>): JSX.Element {
+  const { expenseDate, expenseTitle, expenseAmount } = props;
+  const formattedDate: string = expenseDate.toLocaleString("en-GB", {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
+  });
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{formattedDate}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">£{expenseAmount}</div>
       </div>
     </div>
   );
