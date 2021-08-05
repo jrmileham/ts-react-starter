8f58e4d8a3b3c3bb40922656f40c3eb2737cb891
Johnny Mileham <johnny.mileham@unmind.com>
2021-08-05 16:04:44 +0100

    Added list filtering with conditional rendering

--- src/expense/components/Expenses.tsx
+++ src/expense/components/Expenses.tsx
@@ -1,25 +1,35 @@
-import './Expenses.scss';
-import ExpenseItem  from '@App/expense/components/ExpenseItem';
+import { PropsWithChildren, useState } from 'react';
 import Card from '@App/components/Card';
-import { PropsWithChildren } from 'react';
+import ExpenseItem  from '@App/expense/components/ExpenseItem';
+import ExpensesFilter from '@App/expense/components/ExpensesFilter';
 import Expense from '@App/expense/model/Expense';
+import './Expenses.scss';
 
 export type ExpensesProps = {
   expenses: Expense[];
 };
 
 function Expenses(props: PropsWithChildren<ExpensesProps>): JSX.Element {
+  const [filteredYear, setFilteredYear] = useState<number>(2021);
+
+  const filterChangeHandler = (selectedYear: number): void =>  setFilteredYear(selectedYear);
+
+  const filteredExpenses: Expense[] = props.expenses.filter((expense: Expense) => expense.date.getFullYear() === filteredYear);
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {
-        props.expenses.map((expense: Expense, index: number): JSX.Element => 
-          <ExpenseItem 
-            key={index} 
-            expenseAmount={expense.amount} 
-            expenseTitle={expense.title} 
-            expenseDate={expense.date} 
-          />
-        )
+        (filteredExpenses.length > 0) ?
+          filteredExpenses.map((expense: Expense, index: number): JSX.Element => 
+            <ExpenseItem 
+              key={index} 
+              expenseAmount={expense.amount} 
+              expenseTitle={expense.title} 
+              expenseDate={expense.date} 
+            />
+          )
+        : null
       }
     </Card>
   );
