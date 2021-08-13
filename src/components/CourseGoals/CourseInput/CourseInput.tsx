c18c290a51a995462685474f372dbbb6c1ee609d
Johnny Mileham <johnny.mileham@unmind.com>
2021-08-13 15:13:14 +0100

    Added dynamic inline styles

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -8,22 +8,36 @@ function CourseInput (props): JSX.Element {
   const defaultState: string = "";
 
   const [enteredValue, setEnteredValue] = useState<string>(defaultState);
+  const [isValid, setIsValid] = useState<boolean>(true);
 
   const goalInputChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
     setEnteredValue(event.target.value);
+    if(enteredValue.trim().length > 0) setIsValid(true);
   };
 
   const formSubmitHandler = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0) {
+      setIsValid(false);
+      setTimeout(() => setIsValid(true), 1000);
+      return;
+    }
+    setIsValid(true);
     props.onAddGoal(enteredValue);
     setEnteredValue(defaultState);
   };
 
+  const isValidColor: string = !isValid? 'firebrick' : 'black';
+
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
+        <label style={{color: isValidColor}}>Course Goal</label>
+        <input 
+          style={{borderColor: isValidColor, background: !isValid? 'lightpink' : 'transparent'}} 
+          type="text" onChange={goalInputChangeHandler} 
+          value={enteredValue}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
